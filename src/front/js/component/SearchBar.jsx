0b02a98c7d153cb1c42787beefd00ce3eda50d03
Karai2mil <128420458+Karai2mil@users.noreply.github.com>
2023-08-03 01:33:06 +0000

    Front y back 1.0 de buscador y vista de explorar

---
+++ src/front/js/component/SearchBar.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef, useContext } from 'react'
+import { Context } from '../store/appContext';
+import { useNavigate } from 'react-router-dom';
+import "../../styles/searchbar.css";
+
+
+const SearchBar = () => {
+
+	const navigate = useNavigate()
+	const [isExpanded, setIsExpanded] = useState(false);
+	const [loadingIsActive, setLoadingIsActive] = useState(false)
+	const [searchContent, setSearchContent] = useState('')
+	const searchRef = useRef(null);
+	const { store, actions } = useContext(Context)
+
+	useEffect(() => {
+		const handleOutsideClick = (event) => {
+			if (searchRef.current && !searchRef.current.contains(event.target)) {
+				setIsExpanded(false);
+			}
+		};
+
+		document.addEventListener("click", handleOutsideClick);
+
+		return () => {
+			document.removeEventListener("click", handleOutsideClick);
+		};
+	}, []);
+
+	const handleExpandedSearch = (value) => {
+		// clearTimeout(timerId); // Limpiamos el temporizador existente (si lo hay)
+		// // setLoadingIsActive(true)
+
+		// const timerId = setTimeout(() => {
+		// 	// return setLoadingIsActive(false)
+		// }, 3000);
+		setSearchContent(value)
+	}
+
+	const handleSearchClick = () => {
+		actions.expandedSearch(searchContent);
+		navigate('/explorer')
+	}
+
+	return (
+		<div>
+			<div className={`input-group ${isExpanded ? "expanded" : ""}`} ref={searchRef}>
+				<input
+					id="search-input"
+					className={`search-click border-start-0 ${isExpanded ? "expanded" : ""}`}
+					type="search"
+					placeholder="Buscar artistas, álbumes y otros..."
+					aria-label="Search"
+					onChange={(e) => handleExpandedSearch(e.target.value)}
+					value = {searchContent}
+				/>
+				<span
+					id="search-icon"
+					className="search-icon input-group-text bg-white border-end-0"
+					onClick={() => handleSearchClick()}
+				>
+					<i className="fa-solid fa-magnifying-glass"></i>
+				</span>
+			</div>
+			{/* {loadingIsActive && <div className="loading-icon"></div>} */}
+		</div>
+	)
+}
+
+
+export default SearchBar
+
+
+
+
+
---
+++ src/front/js/pages/Explorer.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useContext } from 'react'
+import { Context } from '../store/appContext'
+import styles from "../../styles/Explorer.module.css";
+import ArticleCard from '../component/ArticleCardExplorer.jsx';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
+
+
+const Explorer = () => {
+
+    const { store, actions } = useContext(Context)
+
+    useEffect(() => {
+        actions.getAllArticles()
+    }, [])
+
+
+
+    return (
+        <div id='general_div' className={styles.generalDiv}>
+            <div id='filter_center' className={styles.filterCenter}>
+                <div id='genres_filters'>
+                    <p><strong>Género</strong></p>
+                    <p>Rock</p>
+                    <p>Electronica</p>
+                    <p>Pop</p>
+                </div>
+                <div id='styles_filters'>
+                    <p><strong>Estilo</strong></p>
+                    <p>House</p>
+                    <p>Techno</p>
+                </div>
+                <div id='country_filters'>
+                    <p><strong>Pais</strong></p>
+                    <p>Alemania</p>
+                    <p>Uruguay</p>
+                </div>
+            </div>
+            <div id='content_center' className={styles.contentCenter}>
+                <div id='upper_filter'>
+                    <ul className={styles.upperFilter}>
+                        <li>Todo</li>
+                        <li style={{ paddingLeft: '100px' }}>Publicaciones</li>
+                        <li style={{ paddingLeft: '100px' }}>Artistas</li>
+                    </ul>
+                </div>
+                <h4>Explorar articulos y artistas</h4>
+                <div id='content'>
+                    <ul className={styles.content}>
+                        {store.on_filtered_or_explorer &&
+                            store.explorer_articles.map((element, index) => {
+                                const [artist, title] = element.titulo.split(' - ')
+                                return (
+                                    <li key={index}>
+                                        <ArticleCard
+                                            key={index}
+                                            title={title}
+                                            artist={artist}
+                                            url_imagen={process.env.BACKEND_URL + "api/utils/images/" + element.url_imagen}
+                                        />
+                                    </li>
+                                )
+                            })
+                        }
+                        {!store.on_filtered_or_explorer &&
+                            store.filtered_explorer_articles.map((element, index) => {
+                                const [artist, title] = element.titulo.split(' - ')
+                                return (
+                                    <li key={index}>
+                                        <ArticleCard
+                                            key={index}
+                                            title={title}
+                                            artist={artist}
+                                            url_imagen={process.env.BACKEND_URL + "api/utils/images/" + element.url_imagen}
+                                        />
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Explorer
